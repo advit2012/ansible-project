af075f52e4faea7ff52a456edacc30cfbc799c4f
manjeet <manjeetsinghlpu47@gmail.com>
2024-07-20 10:57:11 +0530

    first commit

---
+++ src/main/java/com/example/ansible_demo/AnsibleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ansible_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ansible_demo/AnsibleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ansible_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
